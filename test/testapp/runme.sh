29b02984512f5f4f05909a7bece4a99f433e7ab1
Robert Kleniewski <robert.kleniewski@gmail.com>
2017-10-29 00:56:05 +0200

    fun with app

--- test/testapp/runme.sh
+++ test/testapp/runme.sh
@@ -2,13 +2,18 @@ set -e
 eval $(minikube docker-env)
 docker build -t test-node:v1 .
 docker images
-kubectl run test-node --image=test-node:v1 --port=3000
-kubectl get deployments
-kubectl get pods
-kubectl get events
-kubectl expose deployment test-node --type=LoadBalancer
-kubectl get services
-kubectl scale deployment test-node --replicas=5
-kubectl get deployments
-minikube service test-node
+kubectl create -f test-node.yaml
+kubectl create -f test-node-srvc.yaml
+
+
+#kubectl run test-node --image=test-node:v1 --port=3000
+#kubectl get deployments
+#kubectl get pods
+#kubectl get events
+#kubectl expose deployment test-node --type=LoadBalancer
+#kubectl get services
+#kubectl scale deployment test-node --replicas=5
+#kubectl get deployments
+
+minikube service test-node-srvc
 
